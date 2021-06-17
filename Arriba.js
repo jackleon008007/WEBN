e997ddd7f1d389403cd741ba48ade80edc180a81
Jack <jack008007z@outlook.es>
2021-06-17 17:57:24 -0500

    v

---
+++ Arriba.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $('.Inicio').click(function (){
+        $('body,html').animate({
+            scrollTop: '0px'
+        });
+    })
+
+    $('.nuestrosServicios').click(function (){
+        $('body,html').animate({
+            scrollTop: '785px'
+        });
+    })
+    $('.NuestroTrabajo').click(function (){
+        $('body,html').animate({
+            scrollTop: '2850px'
+        });
+    })
+    $('.Contactanos').click(function (){
+        $('body,html').animate({
+            scrollTop: '4000px'
+        });
+    })
+})
